e355a42ff0c0e90f768f7d3036308e4967fea16d
Chao Zhang <charles.zhangzs@gmail.com>
2020-10-06 15:21:30 +0200

    Add back sql script removed by accident

---
+++ week06/pa01/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS movie_reviews (	
+    id              int auto_increment,	
+    review_rating   text,	
+    review_content  text,	
+    PRIMARY KEY (id)	
+) CHARACTER SET = utf8;
