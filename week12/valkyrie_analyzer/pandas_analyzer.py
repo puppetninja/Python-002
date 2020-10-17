8cfe9a6577481991e3281a96bf3b2ae3ea9af544
Chao Zhang <charles.zhangzs@gmail.com>
2020-10-18 00:52:57 +0200

    Wrap up Valkyrie

--- week12/valkyrie_analyzer/pandas_analyzer.py
+++ week12/valkyrie_analyzer/pandas_analyzer.py
@@ -47,10 +47,7 @@ def load_raw_data(db_info):
 
 def cleanse_phones_comments(comments_df):
     # 去重
-    comments_df.drop_duplicates(
-        subset=['phone_name', 'comment_content', 'comment_date'],
-        inplace=True
-    )
+    comments_df.drop_duplicates(subset=['comment_content'], inplace=True)
 
     # 去除空值
     comments_df['phone_name'].replace('', np.nan, inplace=True)
@@ -58,6 +55,8 @@ def cleanse_phones_comments(comments_df):
     comments_df.dropna(
         subset=['phone_name', 'comment_content', 'comment_date'],
         how='any', inplace=True)
+
+    write_df_to_sql(comments_df, 'phone_comments_cleansed', DB_INFO)
 
 
 def write_df_to_sql(df, table, db_info):
@@ -74,6 +73,7 @@ def write_df_to_sql(df, table, db_info):
 
 def cal_phone_emo_trends(phones_df, comments_df):
     phone_opinions = []
+    phone_id = 1
     for _, p_row in phones_df.iterrows():
         phone_name = p_row['phone_name']
         phone_comments = comments_df[comments_df['phone_name'] == phone_name]
@@ -83,10 +83,12 @@ def cal_phone_emo_trends(phones_df, comments_df):
         neg_ratio = len(neg_emos) / len(comments_emo)
 
         phone_opinions.append({
+            'id': phone_id,
             'phone_name': phone_name,
-            'positive': format(1 - neg_ratio, '.3f'),
-            'negative': format(neg_ratio, '.3f')
+            'positive': format((1 - neg_ratio)*100, '.3f'),
+            'negative': format(neg_ratio*100, '.3f')
         })
+        phone_id += 1
 
     df = pd.DataFrame(phone_opinions)
     write_df_to_sql(df, 'phone_opinions', DB_INFO)
--- week12/valkyrie_app/valkyrie_app/settings.py
+++ week12/valkyrie_app/valkyrie_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'valkyrie_phones'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'valkyrie_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'valkyrie',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
